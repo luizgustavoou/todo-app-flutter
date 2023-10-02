cf68c2aa39df83fae3fed9fc7c802a91fe403890
luizgustavoou <luiz.umbelino@nuplam.ufrn.br>
2023-10-01 21:41:20 -0300

    criar todo event

---
+++ todo_frontend/lib/todo/bloc/todo_events.dart
@@ -0,0 +1,9 @@
+abstract class TodoEvent {}
+
+class TodoLoadEvent extends TodoEvent {}
+
+class TodoAddEvent extends TodoEvent {}
+
+class TodoRemoveEvent extends TodoEvent {}
+
+class TodoUpdateEvent extends TodoEvent {}
